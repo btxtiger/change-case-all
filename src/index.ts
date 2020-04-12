ddeabf0325c5d8fa7f5d9fd1ee0105b390b81fab
Jonas <btxtiger@outlook.com>
2020-04-13 01:27:20 +0200

    Fix exports

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
-export * from 'change-case';
-export * from 'lower-case';
-export * from 'lower-case-first';
-export * from 'sponge-case';
-export * from 'swap-case';
-export * from 'title-case';
-export * from 'upper-case';
-export * from 'upper-case-first';
-export * from 'is-upper-case';
-export * from 'is-lower-case';
+import changeCase from 'change-case';
+
+import lowerCase1 from 'lower-case';
+import lowerCaseFirst1 from 'lower-case-first';
+import spongeCase1 from 'sponge-case';
+import swapCase1 from 'swap-case';
+import titleCase1 from 'title-case';
+import upperCase1 from 'upper-case';
+import upperCaseFirst1 from 'upper-case-first';
+import isUpperCase1 from 'is-upper-case';
+import isLowerCase1 from 'is-lower-case';
+
+// Core
+export const camelCase = changeCase.camelCase;
+export const capitalCase = changeCase.capitalCase;
+export const constantCase = changeCase.constantCase;
+export const dotCase = changeCase.dotCase;
+export const headerCase = changeCase.headerCase;
+export const noCase = changeCase.noCase;
+export const paramCase = changeCase.paramCase;
+export const pascalCase = changeCase.pascalCase;
+export const pathCase = changeCase.pathCase;
+export const sentenceCase = changeCase.sentenceCase;
+export const snakeCase = changeCase.snakeCase;
+
+// Extended
+export const lowerCase = lowerCase1.lowerCase;
+export const localeLowerCase = lowerCase1.localeLowerCase;
+export const lowerCaseFirst = lowerCaseFirst1.lowerCaseFirst;
+export const spongeCase = spongeCase1.spongeCase;
+export const swapCase = swapCase1.swapCase;
+export const titleCase = titleCase1.titleCase;
+export const upperCase = upperCase1.upperCase;
+export const localeUpperCase = upperCase1.localeUpperCase;
+export const upperCaseFirst = upperCaseFirst1.upperCaseFirst;
+export const isUpperCase = isUpperCase1.isUpperCase;
+export const isLowerCase = isLowerCase1.isLowerCase;
