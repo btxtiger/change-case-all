09827b17d639819a4efbf78ab396c589e38ceb42
Jonas <btxtiger@icloud.com>
2021-03-15 20:01:04 +0100

    Add unit tests

---
+++ tests/core.test.ts
@@ -0,0 +1,76 @@
+import {
+   camelCase,
+   capitalCase,
+   constantCase,
+   dotCase,
+   headerCase,
+   noCase,
+   paramCase,
+   pascalCase,
+   pathCase,
+   sentenceCase,
+   snakeCase,
+} from '../dist';
+import { SpecReporter } from 'jasmine-spec-reporter';
+
+// @ts-ignore
+jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: 'pretty' } }));
+
+describe('The core modules of change-case', () => {
+   let expression = 'MyName-is_peter parker__and--I-amCool';
+
+   it('should camelCase the expression', () => {
+      expect(camelCase).toBeDefined();
+      expect(camelCase(expression)).toEqual('myNameIsPeterParkerAndIAmCool');
+   });
+
+   it('should capitalCase the expression', () => {
+      expect(capitalCase).toBeDefined();
+      expect(capitalCase(expression)).toEqual('My Name Is Peter Parker And I Am Cool');
+   });
+
+   it('should constantCase the expression', () => {
+      expect(constantCase).toBeDefined();
+      expect(constantCase(expression)).toEqual('MY_NAME_IS_PETER_PARKER_AND_I_AM_COOL');
+   });
+
+   it('should dotCase the expression', () => {
+      expect(dotCase).toBeDefined();
+      expect(dotCase(expression)).toEqual('my.name.is.peter.parker.and.i.am.cool');
+   });
+
+   it('should headerCase the expression', () => {
+      expect(headerCase).toBeDefined();
+      expect(headerCase(expression)).toEqual('My-Name-Is-Peter-Parker-And-I-Am-Cool');
+   });
+
+   it('should noCase the expression', () => {
+      expect(noCase).toBeDefined();
+      expect(noCase(expression)).toEqual('my name is peter parker and i am cool');
+   });
+
+   it('should paramCase the expression', () => {
+      expect(paramCase).toBeDefined();
+      expect(paramCase(expression)).toEqual('my-name-is-peter-parker-and-i-am-cool');
+   });
+
+   it('should pascalCase the expression', () => {
+      expect(pascalCase).toBeDefined();
+      expect(pascalCase(expression)).toEqual('MyNameIsPeterParkerAndIAmCool');
+   });
+
+   it('should pathCase the expression', () => {
+      expect(pathCase).toBeDefined();
+      expect(pathCase(expression)).toEqual('my/name/is/peter/parker/and/i/am/cool');
+   });
+
+   it('should sentenceCase the expression', () => {
+      expect(sentenceCase).toBeDefined();
+      expect(sentenceCase(expression)).toEqual('My name is peter parker and i am cool');
+   });
+
+   it('should snakeCase the expression', () => {
+      expect(snakeCase).toBeDefined();
+      expect(snakeCase(expression)).toEqual('my_name_is_peter_parker_and_i_am_cool');
+   });
+});
---
+++ tests/extended.test.ts
@@ -0,0 +1,77 @@
+import { SpecReporter } from 'jasmine-spec-reporter';
+import {
+   isLowerCase,
+   isUpperCase,
+   localeLowerCase,
+   localeUpperCase,
+   lowerCase,
+   lowerCaseFirst,
+   spongeCase,
+   swapCase,
+   titleCase,
+   upperCase,
+   upperCaseFirst,
+} from '../dist';
+
+// @ts-ignore
+jasmine.getEnv().addReporter(new SpecReporter({ spec: { displayStacktrace: 'pretty' } }));
+
+describe('The extended modules of change-case', () => {
+   let expression = 'MyName-is_peter parker__and--I-amCool';
+
+   it('should camelCase the expression', () => {
+      expect(lowerCase).toBeDefined();
+      expect(lowerCase(expression)).toEqual('myname-is_peter parker__and--i-amcool');
+   });
+
+   it('should localeLowerCase the expression', () => {
+      expect(localeLowerCase).toBeDefined();
+      expect(localeLowerCase(expression, 'EN')).toEqual('myname-is_peter parker__and--i-amcool');
+   });
+
+   it('should lowerCaseFirst the expression', () => {
+      expect(lowerCaseFirst).toBeDefined();
+      expect(lowerCaseFirst(expression)).toEqual('myName-is_peter parker__and--I-amCool');
+   });
+
+   it('should spongeCase the expression', () => {
+      expect(spongeCase).toBeDefined();
+      // Result varies
+      // expect(spongeCase(expression)).toEqual('mynAme-iS_pETEr paRkER__ANd--i-AMCool');
+   });
+
+   it('should swapCase the expression', () => {
+      expect(swapCase).toBeDefined();
+      expect(swapCase(expression)).toEqual('mYnAME-IS_PETER PARKER__AND--i-AMcOOL');
+   });
+
+   it('should titleCase the expression', () => {
+      expect(titleCase).toBeDefined();
+      expect(titleCase(expression)).toEqual('MyName-Is_peter Parker__and--I-amCool');
+   });
+
+   it('should upperCase the expression', () => {
+      expect(upperCase).toBeDefined();
+      expect(upperCase(expression)).toEqual('MYNAME-IS_PETER PARKER__AND--I-AMCOOL');
+   });
+
+   it('should localeUpperCase the expression', () => {
+      expect(localeUpperCase).toBeDefined();
+      expect(localeUpperCase(expression, 'EN')).toEqual('MYNAME-IS_PETER PARKER__AND--I-AMCOOL');
+   });
+
+   it('should upperCaseFirst the expression', () => {
+      expect(upperCaseFirst).toBeDefined();
+      expect(upperCaseFirst(expression)).toEqual('MyName-is_peter parker__and--I-amCool');
+   });
+
+   it('should test expression isUpperCase', () => {
+      expect(isUpperCase).toBeDefined();
+      expect(isUpperCase(expression)).toBeFalse();
+   });
+
+   it('should test expression isLowerCase', () => {
+      expect(isLowerCase).toBeDefined();
+      expect(isLowerCase(expression)).toBeFalse();
+   });
+});
